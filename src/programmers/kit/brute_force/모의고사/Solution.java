712a2df0d872ce229eee81a75daf63eae5d4a73b
parkgeonwook <rjsdnr6331@gmail.com>
2026-06-18 10:14:55 +0900

    완전탐색: 모의고사 개선v1

--- src/programmers/kit/brute_force/모의고사/Solution.java
+++ src/programmers/kit/brute_force/모의고사/Solution.java
@@ -8,35 +8,21 @@ class Solution {
         int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-        // arr은 학생이 제출한 답안.
-        int[][] arr = new int[3][answers.length];
+        int[] score = new int[3];
         for(int i = 0; i<answers.length; i++) {
-            arr[0][i] = one[i % 5];
-        }
-        for(int i = 0; i<answers.length; i++) {
-            arr[1][i] = two[i % 8];
-        }
-        for(int i = 0; i<answers.length; i++) {
-            arr[2][i] = three[i % 10];
+            if(answers[i] == one[i % one.length])
+                score[0]++;
+            if(answers[i] == two[i % two.length])
+                score[1]++;
+            if(answers[i] == three[i % three.length])
+                score[2]++;
         }
 
-        int[] result = new int[3];
-        for(int i = 0; i<3; i++) {
-            int correct = 0;
-            for(int j=0; j<answers.length; j++) {
-                if(answers[j] == arr[i][j]) {
-                    correct++;
-                }
-            }
-            result[i] = correct;
-        }
-
-        int max = Math.max(result[0], result[1]);
-        max = Math.max(max, result[2]);
+        int max = Math.max(score[0], Math.max(score[1], score[2]));
 
         List<Integer> list = new ArrayList<>();
         for(int i = 0; i<3; i++) {
-            if(max == result[i])
+            if(max == score[i])
                 list.add(i+1);
         }
 
